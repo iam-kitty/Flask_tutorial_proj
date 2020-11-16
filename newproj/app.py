b754af9bd312a3f3659feef7bce0f24e68913f68
Your Nameiam-kitty <keerthikumar6995@gmail.com>
2020-11-16 11:50:07 +0530

    including Bootstrap

--- newproj/app.py
+++ newproj/app.py
@@ -1,27 +1,57 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
-all_posts = [
-    {
-        'title':'post1',
-        'content':'this is the content of post 1, LALALALA.',
-        'author':'kitty'
-    },
-    {
-        'title':'post1',
-        'content':'this is the content of post 2, LALALALA'
-    }
-]
 
-@app.route('/home')
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20),nullable=False,default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog Post' + str(self.id)
+
+
+
+"""  **dummy data**     all_posts = [
+                        {
+                            'title':'post1',
+                            'content':'this is the content of post 1, LALALALA.',
+                            'author':'kitty'
+                        },
+                        {
+                            'title':'post1',
+                            'content':'this is the content of post 2, LALALALA'
+                        }
+                    ]
+
+"""
+@app.route('/')
 def index():
 #return 'Home Page' #<h1>Home page </h1>
     return render_template('index.html')
 
-@app.route('/post')
+@app.route('/posts', methods=['GET','POST'])
 def posts():
-    return render_template('posts.html',posts = all_posts )
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title,content=post_content,author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('posts')
+
+    else:
+
+        all_posts = (BlogPost.query.order_by(BlogPost.date_posted).all())
+        return render_template('posts.html',posts = all_posts )
 
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
@@ -32,7 +62,24 @@ def Hello(name,id):
 def get_req():
     return " you'll only get"
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
+@app.route('/posts/edit/<int:id>',methods=['GET','POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html',post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
